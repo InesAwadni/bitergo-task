db7c5c279a70154c61710c400340c73c375b53ee
InesAwadni <ines.awadni9@gmail.com>
2022-09-25 14:18:10 +0200

    create findAll, deleteAll and getAverageAge methods

--- task/src/main/java/com/bitergo/task/person/PersonRepository.java
+++ task/src/main/java/com/bitergo/task/person/PersonRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PersonRepository extends CrudRepository<Person,Long> {
     @Query("SELECT AVG(age) FROM Person")
-    int getAverageAge();
+    float getAverageAge();
 }
--- task/src/main/java/com/bitergo/task/person/PersonService.java
+++ task/src/main/java/com/bitergo/task/person/PersonService.java
@@ -1,4 +1,35 @@
 package com.bitergo.task.person;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PersonService {
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = (List<Person>) personRepository.findAll();
+        return persons;
+    }
+
+    public float getAverageAge(){
+        var averageAge = personRepository.getAverageAge();
+        return  averageAge;
+    }
+
+    public boolean deleteAll (){
+        personRepository.deleteAll();
+
+        List<Person> persons = (List<Person>) personRepository.findAll();
+        if(persons.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
 }
