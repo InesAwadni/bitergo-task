0af51d1097f8a2665b6788f9f7c36c5919e0e47f
InesAwadni <ines.awadni9@gmail.com>
2022-09-25 14:11:19 +0200

    configure database and insert data into database

---
+++ task/src/main/resources/data-postgres.sql
@@ -0,0 +1,4 @@
+INSERT INTO person(name, age) VALUES('Alex', 20);
+INSERT INTO person(name, age) VALUES('Emma', 30);
+INSERT INTO person(name, age) VALUES('Mario', 50);
+INSERT INTO person(name, age) VALUES('Melanie', 26);
